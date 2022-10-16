cda222273e8f1ae5ebfa21f4c2e2b4b7a596ff3b
Nid silva <80358224+Ivoneide01@users.noreply.github.com>
2022-10-15 22:41:11 -0300

    add
    calculadora em java

---
+++ aula/Aula.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula;
+
+import java.time.DayOfWeek;
+
+/**
+ *
+ * @author neidi
+ */
+public class Aula {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int numero = 50;
+        int segundoNumero =  40 + ++numero;
+        System.out.println(segundoNumero);
+        System.out.println(numero);
+    }
+    
+}
